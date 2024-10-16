34abbb547af9487f76a4a169377fbd7658226dd7
shao.chenzhe <shao.chenzhe@hzleaper.com>
2024-10-16 14:32:16 +0800

    chore: use electron-vite-plugin to run dev

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import electronVitePlugin from 'vite-plugin-electron/simple';
+import pkg from './package.json';
+import { rmSync } from 'fs';
 
-export default defineConfig({
-    base: './',
-    plugins: [react()],
-    server: {
-        port: 9090,
-        watch: {
-            usePolling: true, // 修复HMR热更新失效
+export default defineConfig(({ command }) => {
+    rmSync('dist-electron', { recursive: true, force: true });
+    const isServe = command === 'serve';
+    const isBuild = command === 'build';
+    const sourcemap = isServe;
+
+    return {
+        plugins: [
+            react(),
+            electronVitePlugin({
+                main: {
+                    entry: './src/index.js',
+                    vite: {
+                        build: {
+                            sourcemap,
+                            minify: isBuild,
+                            outDir: 'dist-electron',
+                            rollupOptions: {
+                                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
+                            },
+                        },
+                    },
+                },
+            }),
+        ],
+        server: {
+            port: 9090,
+            watch: {
+                usePolling: true, // 修复HMR热更新失效
+            },
+            host: true,
         },
-        host: true,
-    },
+    };
 });
